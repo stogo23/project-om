2040954341a1d66ab2fd834e229e7f3b3febc072
nouroudine <nouroudine.abdou@epitech.eu>
2024-09-05 20:25:48 +0200

    save4

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Service\OmdbApiService;
 
@@ -16,6 +17,13 @@ class FilmController extends AbstractController
 {
     #[Route('/film', name: 'app_film_index', methods: ['GET'])]
     public function index(FilmRepository $filmRepository): Response
+    {
+        return $this->render('film/index.html.twig', [
+            'films' => $filmRepository->findAll(),
+        ]);
+    }
+    #[Route('/', name: 'app_film_inde', methods: ['GET'])]
+    public function inde(FilmRepository $filmRepository): Response
     {
         return $this->render('film/index.html.twig', [
             'films' => $filmRepository->findAll(),
@@ -43,12 +51,20 @@ class FilmController extends AbstractController
     }
 
     #[Route('/film/{id}', name: 'app_film_show', methods: ['GET'])]
-    public function show(Film $film): Response
+    public function show(int $id, FilmRepository $filmRepository): Response
     {
+        $film = $filmRepository->find($id);
+    
+        
+        if (!$film) {
+            throw $this->createNotFoundException('Le film demandé n\'existe pas.');
+        }
+    
         return $this->render('film/show.html.twig', [
             'film' => $film,
         ]);
     }
+    
 
     #[Route('/film/{id}/edit', name: 'app_film_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Film $film, EntityManagerInterface $entityManager): Response
@@ -96,35 +112,40 @@ class FilmController extends AbstractController
     }
     
     #[Route('/search', name: 'app_film_search', methods: ['GET', 'POST'])]
-    public function search(Request $request): Response
+    public function search(Request $request, EntityManagerInterface $entityManager): Response
     {
-      try {
         $title = $request->query->get('title');
         $movieData = null;
-
+    
         if ($title) {
             $movieData = $this->omdbApiService->fetchMovieData($title);
-        }else{
-            
-            $movieData = null;
-            return $this->render('film/search.html.twig', [
-                'movie' => $movieData,
-                'title' => $title,
-            ]);
         }
-
+    
+        // Créer le formulaire FilmType pour l'ajout de films
+        $film = new Film();
+        $form = $this->createForm(FilmType::class, $film);
+        $form->handleRequest($request);
+    
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($film);
+            $entityManager->flush();
+    
+            $this->addFlash('success', 'Film ajouté avec succès');
+            return $this->redirectToRoute('app_film_index');
+        }
+    
+        // Renvoyer les données de la recherche et le formulaire
         return $this->render('film/search.html.twig', [
             'movie' => $movieData,
             'title' => $title,
+            'form' => $form->createView(), // Passer le formulaire à la vue
         ]);
-      } catch (\Throwable $th) {
-        $movieData = null;
-        return $this->render('film/search.html.twig', [
-            'movie' => $movieData,
-            'title' => $title,
-        ]);
-      }
     }
+
+  
+
+    
+
 
 }
     
